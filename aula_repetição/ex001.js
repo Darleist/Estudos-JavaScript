3d80a737646aeaafe98ce8f6b84283a19e305ca3
Darlei Strege <darleistrege.py@gmail.com>
2022-04-27 10:35:20 -0400

    Estudando

---
+++ aula_repetição/ex001.js
@@ -0,0 +1,17 @@
+/* var c = 1
+while (c <= 6) {
+    console.log (`Tudo bem? ${c}`)
+    c++
+}
+
+
+var c=1
+do {
+    console.log (`Estudando ${c}`)
+    c++
+} while (c <=3) */
+
+for (var c=1; c<=10; c++) {
+    console.log (`Contando ${c}`)
+}
+console.log ('Fim!')

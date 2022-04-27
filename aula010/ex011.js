3d15c38d06513a00a747a74669c471e4060ccc3b
Darlei Strege <darleistrege.py@gmail.com>
2022-04-27 09:30:08 -0400

    Treino

---
+++ aula010/ex011.js
@@ -0,0 +1,8 @@
+var idade =20
+console.log (`Voce tem ${idade} anos`)
+if (idade < 16 ) {
+    console.log ('Não vota')
+} else if  (idade < 18 || idade > 65) {
+    console.log ('Voto opcional') } 
+else {
+    console.log ('Voto obrigatorio') }  
